1acaf15723ce2488d5d0d8d583ee35348b84e4b0
wwlee94 <wwlee94@naver.com>
2020-01-03 00:24:05 +0900

    Products 컨트롤러 index 추가

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,22 @@
 class ProductsController < ApplicationController
   # GET /products
   def index
-    render json: JSON.pretty_generate('hello index');
+    dynamodb = Aws::DynamoDB::Resource.new(region: 'ap-northeast-2')
+    table = dynamodb.table('hwahae-api-dev-product')
+
+    scan_output = table.scan({
+      limit: 10,
+      select: "ALL_ATTRIBUTES"
+    })
+
+    scan_output.items.each do |item|
+        keys = item.keys
+        keys.each do |k|
+            puts "#{k} : #{item[k]}"
+            puts item[k].class
+        end
+    end
+    render json: JSON.pretty_generate(scan_output.items);
   end
 
   # GET /products/1
