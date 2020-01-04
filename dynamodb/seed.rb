ef3e823d2ef8a2b9e25704de3c65da339fbb78e9
wwlee94 <wwlee94@naver.com>
2020-01-04 19:58:57 +0900

    seed 데이터 스킨타입 score 컬럼 및 상세설명 추가

--- dynamodb/seed.rb
+++ dynamodb/seed.rb
@@ -1,18 +1,35 @@
 require 'csv'
 require 'aws-sdk'
 
+GREPP_S3_URL = 'https://grepp-programmers-challenges.s3.ap-northeast-2.amazonaws.com/2020-birdview'
+
+full_size_image = "#{GREPP_S3_URL}/image"
+thumbnail_image = "#{GREPP_S3_URL}/thumbnail"
+
 dynamodb = Aws::DynamoDB::Resource.new(region: 'ap-northeast-2')
 table = dynamodb.table('hwahae-api-dev-product')
 
+
 product_list = CSV.parse(File.read('hwahae-items.csv'), headers: true)
+i = 0
 product_list.each do |product|
+    description = "";
+    for i in 1..10
+        break description += product['name'] if i == 10
+        description += product['name'] +'\n'
+    end
     table.put_item({
         item: {
-            image_id: product["image_id"],
-            url: product["url"],
-            name: product["name"],
-            price: product["price"]
+            image_id: product['image_id'],
+            full_size_image: "#{full_size_image}/#{product['image_id']}.jpg",
+            thumbnail_image: "#{thumbnail_image}/#{product['image_id']}.jpg",
+            name: product['name'],
+            price: product['price'],
+            description: description,
+            oily_score: rand(0..100),
+            dry_score: rand(0..100),
+            sensitive_score: rand(0..100),
         }
     })
-    puts(product["name"] + ' 추가 성공 !!')
+    puts("#{product['name']} 저장 완료 !")
 end
